36131ec884f73fdbaccadc5526ee8a4f2e27bb15
Júlia H Ferreira <148831055+JuliaHFerreira@users.noreply.github.com>
2024-02-05 23:53:57 -0300

    Update Pessoa.java
    arrumando codigo

--- Fixacao_vetores/Fixacao_vetores_EX_3/src/entities/Pessoa.java
+++ Fixacao_vetores/Fixacao_vetores_EX_3/src/entities/Pessoa.java
@@ -6,31 +6,19 @@ public class Pessoa {
 	private int age;
 	private double height;
 	
-	public Pessoa() {
-	}
-	
 	public Pessoa(String name, int age, double height) {
 		this.name = name;
 		this.age = age;
-		this.height = height;
-		
+		this.height = height;	
 	}
+	
 	public String getName() {
 		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
 	}
 	public int getAge() {
 		return age;
 	}
-	public void setAge(int age) {
-		this.age = age;
-	}
 	public double getHeight() {
 		return height;
-	}
-	public void setHeight(double height) {
-		this.height = height;
 	}
 }
