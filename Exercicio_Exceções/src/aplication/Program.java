4a9b8a0e3189d6558b052a53e1826a437e9d9538
JuliaHFerreira <juliahelena317@gmail.com>
2024-04-17 22:39:19 -0300

    Exercicios try catch (exceções)

---
+++ Exercicio_Exceções/src/aplication/Program.java
@@ -0,0 +1,44 @@
+package aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			System.out.println("Enter account data");
+			System.out.print("Number: ");
+			Integer number = sc.nextInt();
+			System.out.print("Holder: ");
+			String holder = sc.nextLine();
+			sc.nextLine();
+			System.out.print("Initial balance: ");
+			Double balance = sc.nextDouble();
+			System.out.print("Withdraw limit: ");
+			Double withdrawLimit = sc.nextDouble();
+			Account account = new Account(number, holder, balance, withdrawLimit);
+			
+			System.out.println();
+			System.out.print("Enter amount for withdraw: ");
+			Double amount = sc.nextDouble();
+			account.withdraw(amount);
+			
+			System.out.printf("New balance: %.2f", account.getBalance());
+		}
+		catch (DomainException e) {
+			System.out.println("Withdraw error: " + e.getMessage());
+		}
+		catch (RuntimeException e) {
+			System.out.println("Unexpected error");
+		}
+		sc.close();
+	}
+}
